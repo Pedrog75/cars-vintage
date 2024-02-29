13fcb173dd0f8f6002b28398c41f9c8e7182a21c
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-02-29 11:21:51 +0100

    modif

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,6 +3,7 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
+    console.log('hello');
     flatpickr(this.element);
   }
 }
